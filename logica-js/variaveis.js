8c12fae87498f53de9a7d2bc1d744578f6c9c73a
PedroHauer <pedrohhcg@gmail.com>
2025-09-09 22:14:15 -0300

    Variáveis em Javascript

---
+++ logica-js/variaveis.js
@@ -0,0 +1,23 @@
+//variável com var
+// var nome = "Pedro"; // String (texto) dentro de aspas=string
+// var nome = "jeferson";
+// nome = "Joao"; // Alterando o valor da variavel
+
+//variável com let
+//let nome = "Pedro"; // String (texto) dentro de aspas=string
+//nome = "Joao"; // Alterando o valor da variavel
+
+//variável em const
+const nome = "Pedro" // String (texto) dentro de aspas=string
+const sobrenome = "Hauer" // String (texto) dentro de aspas=string
+const idade = 20 // Number (número) sem aspas=número
+const masculino = true // Boolean (booleano) true ou false
+
+//array
+const frutas = ["Laranja", "Maçã", "Tangerina", "Uva"]
+
+console.log(frutas[0]) // Acessando o item do array pela posição (começa do 0)
+
+//console.log("Meu nome é " + nome + " " + sobrenome + " e minha idade é " + idade + " anos.") // Concatenando variáveis com texto usando o +
+
+//console.log(`Meu nome é ${nome} ${sobrenome} e minha idade é ${idade} anos.`) // Concatenando variáveis com texto usando template string (crase)

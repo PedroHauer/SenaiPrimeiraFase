e1a387913d612c351dde3dbd5e8b339c5ee7912f
PedroHauer <pedrohhcg@gmail.com>
2025-09-15 22:10:55 -0300

    Exercicios

---
+++ sq05/52.js
@@ -0,0 +1,10 @@
+let A, B 
+A = Number(prompt("Digite o valor de A: "))
+B = Number(prompt("Digite o valor de B: "))
+if (A > B){
+    alert("A é maior que B")
+}else if (A < B){
+    alert("B é maior que A")
+} else {
+    alert("A é igual a B")
+}
---
+++ sq05/53.js
@@ -0,0 +1,12 @@
+let placa = Number(prompt("Digite o ultimo número da placa do veículo: "))
+if(placa == 1 || placa == 0){
+  alert("Não pode circular na segunda-feira")
+}else if(placa == 0 || placa == 3){
+    alert("Não pode circular na terça-feira")
+}else if (placa == 4 || placa == 5){
+    alert("Não pode circular na quarta-feira")
+}else if(placa==6|| placa == 7){
+    alert("Não pode circular na quinta-feira")      
+}else if (placa== 8|| placa == 9){
+    alert("Não pode circular na sexta-feira")
+}

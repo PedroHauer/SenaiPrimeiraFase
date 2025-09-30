01e9df84deede48c3d57c7e1c5c1c8af1a24c6a8
PedroHauer <pedrohhcg@gmail.com>
2025-09-29 22:15:49 -0300

    Repetições

---
+++ sq05/repeticoes/script.js
@@ -0,0 +1,15 @@
+
+// let acertos = Number(prompt("Quantos acertos? "))
+// let valorPorQuestao = 10/16
+// let nota = acertos * valorPorQuestao
+// alert("Sua nota é " + nota)
+
+
+
+
+let contador = 0
+while(contador < 2){
+    contador = contador + 1
+    console.log("Contador é " + contador)
+    
+}

7906e5dfe33e3df6d718a6508c15c7f964b4b480
PedroHauer <pedrohhcg@gmail.com>
2025-09-30 22:13:14 -0300

    FOR, FUNCOES

---
+++ logica-js/for.js
@@ -0,0 +1,37 @@
+// for(let i = 0; i<=10;i++){
+//     console.log(i)
+// }
+
+//for que imprima DE 10 A 0
+
+// for(let i = 10; i >=0; i--){
+//       console.log(i)
+// }
+
+// for(let i = 0; i<=20;i+=2){
+//    console.log(i)
+// }
+
+// let soma = 0
+
+// for(let i =1;i<100;i++){
+//     soma += i
+// }
+
+// console.log("Resultado da soma " , soma)
+
+// let alunos = ["João", "Maria", "Joana"]
+// alunos.push("Pedro")
+
+// for(let i = 0; i<alunos.length; i++){
+//     console.log("aluno: ", alunos[i])
+// }
+
+// console.log(alunos.length)
+
+let frutas = ["Maça", "Laranja", "Melancia", "Uva"]
+frutas.push("Pêra")
+for(let i = 0; i<frutas.length;i++){
+  console.log("Frutas: ", frutas[i])
+}
+
---
+++ logica-js/funcoes.js
@@ -0,0 +1,16 @@
+// function saudacao(nome){
+//     console.log(`Bom dia ${nome}`)
+// }
+
+// const saudacao = (nome)=>{
+//     console.log(`Bom dia, ${nome}`)
+// }
+
+// saudacao("Justin")
+// saudacao("Bieber")
+
+const soma = (x,y) => x+y
+
+
+
+console.log(`A soma é ${soma(10,10)}`)

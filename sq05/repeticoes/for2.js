d02ea2e1fba84ab296fdb48ed47020c1e1ca87ce
PedroHauer <pedrohhcg@gmail.com>
2025-10-02 21:30:58 -0300

    Exercicios de repetição

---
+++ sq05/repeticoes/for2.js
@@ -0,0 +1,49 @@
+
+// let i = 0
+// while(i<3){
+//     alert("Valor do i: " + i)
+//     i++
+// }
+
+// for(let i = 0; i<3; i++){
+//     alert("Valor do i: " + i)
+// }
+
+// let soma = 0
+// for(let i=1;i<11;i++){
+//     let nota = Number(prompt(`Digite a ${i} nota: `))
+//     soma = soma + nota 
+// }
+
+//  alert("Soma: " + soma)
+
+//  const nomes = ["Dona Bete", "Zezinho", "Mariazinha", "Joao", "Justin"]
+
+//  for(let i =0;i<4;i++){
+//      alert(nomes[i])
+//  }
+
+//EX 01 
+
+// for(let i = 1;i<10;i++){
+//      alert(`O quadrado de ${i} é ${i*i}`)
+// }   
+
+//EX 02
+// for(let i = 10;i>0;i--){
+//     alert(i)
+// }
+
+//EX 03 nn entendi 
+
+// for(let i=1;i<=100;i++){
+//     let soma = 0
+//     soma = soma + i
+//     alert(soma)
+// }
+
+// //EX 04 Imprima a tabuada do 5.
+// for(let i=1;i<=10;i++){
+//     alert(`5 x ${i} = ${5*i}`)
+
+// }

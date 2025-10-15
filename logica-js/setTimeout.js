f5e43fe3199ff89174cb5f88841dfa8cb78a4969
PedroHauer <pedrohhcg@gmail.com>
2025-10-14 22:15:05 -0300

    DOM

---
+++ logica-js/setTimeout.js
@@ -0,0 +1,28 @@
+// setTimeout(()=>{
+//     console.log("Executei depois de 3 seg")
+// },3000)
+
+ function saudacao(){
+    console.log("Olá")
+ }
+
+
+
+function imprimeDado(){
+    console.log("imprimir")
+ }
+
+ setTimeout(imprimeDado,5000)
+ saudacao()
+
+ function soma(){
+    console.log(5)
+ }
+ setTimeout(soma,6000)
+
+ setTimeout(()=>{
+   console.log("nostalgia ou amor?")
+ },7000)
+
+
+

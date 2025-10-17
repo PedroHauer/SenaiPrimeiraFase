81a7679bc025ee5ed9976a599d6fb80e7995981a
PedroHauer <pedrohhcg@gmail.com>
2025-10-16 21:55:19 -0300

    Mais For

--- logica-js/for.js
+++ logica-js/for.js
@@ -29,9 +29,79 @@
 
 // console.log(alunos.length)
 
-let frutas = ["Maça", "Laranja", "Melancia", "Uva"]
-frutas.push("Pêra")
-for(let i = 0; i<frutas.length;i++){
-  console.log("Frutas: ", frutas[i])
+// let frutas = ["Maça", "Laranja", "Melancia", "Uva"]
+// frutas.push("Pêra")
+// for(let i = 0; i<frutas.length;i++){
+//   console.log("Frutas: ", frutas[i])
+// }
+
+// for (let i = 0; i<100; i++) {
+//   console.log(i)
+  
+// }
+
+// let i = 0
+
+// do{
+//   console.log(i);i++;
+// }while (i <= 99)
+
+// let dia = "Sexta"
+
+// switch (dia) {
+//   case "Segunda":
+//     console.log("Segundou");
+//     break;
+// case "Sexta":
+//   console.log("Sextou");
+//   break;
+//   default:
+//     console.log("Falha")
+//     break;
+// }
+
+// let dia = 1
+// switch (dia) {
+//   case 1:
+//     console.log("Domingo");
+//     break;
+//   case 2:
+//     console.log("Segunda");
+//     break;
+//     case 3:
+//       console.log("Terça")
+//       break;
+//     case 4:
+//       console.log("Quarta");
+//       break;
+//     case 5:
+//       console.log("Quinta")
+//       break;
+//     case 6:
+//       console.log("Sexta");
+//       break;
+//     case 7:
+//       console.log("Sabado");
+//       break;
+//   default:
+//     console.log("Falha")
+//     break;
+// }
+
+//um programa que calcule o quadrado de cada numero 1 a 10
+
+// for (let i = 1; i<=10; i++){
+//     console.log(i*i)
+// }
+
+//calcule a soma dos números de 1 a 10
+for(let i = 1, soma = 0; i<=10; i++){
+    soma += i   
+    console.log(soma)
 }
+// console.log("Resultado da soma: ", soma)
+// for(let i = 1; i<=10; i++){
+//     console.log(i)
+// }    
+
 

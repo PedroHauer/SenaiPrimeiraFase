1d93d77088fb9fc6a44994d19d40ad26cd9a22ae
PedroHauer <pedrohhcg@gmail.com>
2025-10-21 22:17:39 -0300

    HTML CSS JS

--- logica-js/usuario-admin.js
+++ logica-js/usuario-admin.js
@@ -1,7 +1,7 @@
 let prompt = require("prompt-sync")()
 let usuario = prompt("Digite o nome do usuário: ")
 let senha = Number(prompt("Digite a senha: "))
-if(usuario === "admin" && senha === "12345"){
+if(usuario === "admin" && senha === 12345){
     console.log("Olá admin, seja bem vindo!")
 }else{ 
     console.log("Acesso negado")

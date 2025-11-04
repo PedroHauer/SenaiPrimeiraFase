b32dbb67d468066eb56b149530e809f027c9d4ca
PedroHauer <pedrohhcg@gmail.com>
2025-11-03 22:12:06 -0300

    poucas att

---
+++ dom-manipulacao/dom-revisao/script.js
@@ -0,0 +1,9 @@
+let usuario
+
+function cadastrarUsuario(){
+    //console.log("ate aqui deu")
+    usuario = document.getElementById("inputUsuario").value
+
+    document.getElementById("inputUsuario").value = ''
+    
+}
